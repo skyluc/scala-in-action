60a826a135bf4220bb75fb50e32e022356447c53
Heiko Seeberger <heiko.seeberger@googlemail.com>
2012-10-01 20:53:49 +0200

    Add default arguments and - operator

--- src/main/scala/Time.scala
+++ src/main/scala/Time.scala
@@ -14,11 +14,14 @@
  * limitations under the License.
  */
 
-case class Time(hours: Int, minutes: Int) {
+case class Time(hours: Int = 0, minutes: Int = 0) {
 
   val asMinutes: Int =
     hours * 60 + minutes
 
   def minus(that: Time): Int =
     this.asMinutes - that.asMinutes
+
+  def -(that: Time): Int =
+    minus(that)
 }
